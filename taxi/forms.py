034a9bc966c04c34656c22effe5f55acbcbc456e
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-08-04 11:04:18 +0200

    Solution

--- taxi/forms.py
+++ taxi/forms.py
@@ -6,7 +6,26 @@ from django.core.exceptions import ValidationError
 from taxi.models import Driver, Car
 
 
-class DriverCreationForm(UserCreationForm):
+class LicenseNumberCleanMixin:
+    def clean_license_number(self):
+        license_number = self.cleaned_data["license_number"]
+
+        if len(license_number) != 8:
+            raise ValidationError(
+                "Ensure that value length is equal 8"
+            )
+        if not (license_number[:3].isalpha() and license_number[:3].isupper()):
+            raise ValidationError(
+                "Ensure that the first 3 chars are alphas and are uppers"
+            )
+        if not license_number[3:8].isdigit():
+            raise ValidationError(
+                "Ensure that the last 5 chars are digits"
+            )
+        return license_number
+
+
+class DriverCreationForm(UserCreationForm, LicenseNumberCleanMixin):
     class Meta(UserCreationForm.Meta):
         model = Driver
         fields = UserCreationForm.Meta.fields + (
@@ -15,45 +34,11 @@ class DriverCreationForm(UserCreationForm):
             "license_number",
         )
 
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
 
-        if len(license_number) != 8:
-            raise ValidationError(
-                "Ensure that value length is equal 8"
-            )
-        if not (license_number[:3].isalpha() and license_number[:3].isupper()):
-            raise ValidationError(
-                "Ensure that the first 3 chars are alphas and are uppers"
-            )
-        if not license_number[3:8].isdigit():
-            raise ValidationError(
-                "Ensure that the last 5 chars are digits"
-            )
-        return license_number
-
-
-class DriverLicenseUpdateForm(forms.ModelForm):
+class DriverLicenseUpdateForm(forms.ModelForm, LicenseNumberCleanMixin):
     class Meta:
         model = Driver
         fields = ("license_number",)
-
-    def clean_license_number(self):
-        license_number = self.cleaned_data["license_number"]
-
-        if len(license_number) != 8:
-            raise ValidationError(
-                "Ensure that value length is equal 8"
-            )
-        if not (license_number[:3].isalpha() and license_number[:3].isupper()):
-            raise ValidationError(
-                "Ensure that the first 3 chars are alphas and are uppers"
-            )
-        if not license_number[3:8].isdigit():
-            raise ValidationError(
-                "Ensure that the last 5 chars are digits"
-            )
-        return license_number
 
 
 class CarForm(forms.ModelForm):
