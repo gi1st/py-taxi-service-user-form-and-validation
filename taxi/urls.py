dfb70c83ad2c9fa63dfeccdaeabf88a3d9ee61a5
Yakiv Seleznov <yakov.seleznyov@gmail.com>
2025-08-04 10:18:12 +0200

    Solution

--- taxi/urls.py
+++ taxi/urls.py
@@ -4,11 +4,16 @@ from .views import (
     index,
     CarListView,
     CarDetailView,
+    CarAssignView,
+    CarUnassignView,
     CarCreateView,
     CarUpdateView,
     CarDeleteView,
     DriverListView,
     DriverDetailView,
+    DriverCreateView,
+    DriverUpdateView,
+    DriverDeleteView,
     ManufacturerListView,
     ManufacturerCreateView,
     ManufacturerUpdateView,
@@ -41,10 +46,27 @@ urlpatterns = [
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
     path("cars/create/", CarCreateView.as_view(), name="car-create"),
     path("cars/<int:pk>/update/", CarUpdateView.as_view(), name="car-update"),
+    path("cars/<int:pk>/assign/", CarAssignView.as_view(), name="car-assign"),
+    path(
+        "cars/<int:pk>/unassign/",
+        CarUnassignView.as_view(),
+        name="car-unassign",
+    ),
     path("cars/<int:pk>/delete/", CarDeleteView.as_view(), name="car-delete"),
     path("drivers/", DriverListView.as_view(), name="driver-list"),
     path(
         "drivers/<int:pk>/", DriverDetailView.as_view(), name="driver-detail"
+    ),
+    path("drivers/create/", DriverCreateView.as_view(), name="driver-create"),
+    path(
+        "drivers/<int:pk>/update/",
+        DriverUpdateView.as_view(),
+        name="driver-update",
+    ),
+    path(
+        "drivers/<int:pk>/delete/",
+        DriverDeleteView.as_view(),
+        name="driver-delete",
     ),
 ]
 
--- taxi/views.py
+++ taxi/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
+from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
 
 
 @login_required
@@ -62,15 +63,31 @@ class CarDetailView(LoginRequiredMixin, generic.DetailView):
     model = Car
 
 
+class CarAssignView(LoginRequiredMixin, generic.View):
+    def post(self, request, *args, **kwargs):
+        car = get_object_or_404(Car, pk=kwargs["pk"])
+        driver = request.user
+        driver.cars.add(car)
+        return redirect("taxi:car-detail", pk=car.pk)
+
+
+class CarUnassignView(LoginRequiredMixin, generic.View):
+    def post(self, request, *args, **kwargs):
+        car = get_object_or_404(Car, pk=kwargs["pk"])
+        driver = request.user
+        driver.cars.remove(car)
+        return redirect("taxi:car-detail", pk=car.pk)
+
+
 class CarCreateView(LoginRequiredMixin, generic.CreateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
 class CarUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Car
-    fields = "__all__"
+    form_class = CarForm
     success_url = reverse_lazy("taxi:car-list")
 
 
@@ -87,3 +104,21 @@ class DriverListView(LoginRequiredMixin, generic.ListView):
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
     model = Driver
     queryset = Driver.objects.all().prefetch_related("cars__manufacturer")
+
+
+class DriverCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Driver
+    form_class = DriverCreationForm
+    success_url = reverse_lazy("taxi:driver-list")
+
+
+class DriverUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Driver
+    form_class = DriverLicenseUpdateForm
+    success_url = reverse_lazy("taxi:driver-list")
+
+
+class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Driver
+    template_name = "taxi/driver_confirm_delete.html"
+    success_url = reverse_lazy("taxi:driver-list")
